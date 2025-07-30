e06826c177e0f746f713a47fef995b421c147508
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-07-30 20:57:58 +0700

    Update VF8.java

--- Java-homework5/VF8.java
+++ Java-homework5/VF8.java
@@ -1,40 +1,26 @@
 public class VF8 implements ICar {
     private String licensePlate;
-    private final int numberOfSeats = 7;
+    private int wheels;
+    private int seats;
 
     public VF8(String licensePlate) {
         this.licensePlate = licensePlate;
+        this.wheels = 4;
+        this.seats = 7;
     }
 
-    @Override
-    public void brake() {
-        System.out.println("VF8 is braking...");
+    public void showInfo() {
+        System.out.println("===== VF8 =====");
+        System.out.println("License Plate: " + licensePlate);
+        System.out.println("Wheels: " + wheels);
+        System.out.println("Seats: " + seats);
     }
 
-    @Override
     public void accelerate() {
         System.out.println("VF8 is accelerating...");
     }
 
-    @Override
-    public int getNumberOfWheels() {
-        return 4;
-    }
-
-    @Override
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    @Override
-    public void showInfo() {
-        System.out.println("===== VF8 =====");
-        System.out.println("License Plate: " + licensePlate);
-        System.out.println("Wheels: " + getNumberOfWheels());
-        System.out.println("Seats: " + numberOfSeats);
+    public void brake() {
+        System.out.println("VF8 is braking...");
     }
 }
